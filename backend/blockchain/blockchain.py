9d180018dbf781a757b25d658339199aa5d5c76c
Darshan Khapekar <darshankhapekar8520@gmail.com>
2025-03-02 00:40:04 +0530

    Updates Blockchain to add Redis

--- backend/blockchain/blockchain.py
+++ backend/blockchain/blockchain.py
@@ -1,6 +1,17 @@
 import hashlib
 import time
 import json
+import redis
+import datetime
+
+redis_client = redis.StrictRedis(
+    host="redis-12407.crce182.ap-south-1-1.ec2.redns.redis-cloud.com",  
+    port=12407,  
+    username="default",
+    password="aWHHBWiwKtcPFSx5ECSgsmxUFG164av9",  
+    decode_responses=True
+)
+
 
 class Block:
     def __init__(self, index, timestamp, data, previous_hash=''):
@@ -14,17 +25,63 @@ class Block:
         block_string = f"{self.index}{self.timestamp}{self.data}{self.previous_hash}"
         return hashlib.sha256(block_string.encode()).hexdigest()
 
+
 class Blockchain:
     def __init__(self):
-        self.chain = [self.create_genesis_block()]
+        self.chain = []  # Initialize chain first
+        self.load_chain()  # Load or create genesis block
+        
+    def load_chain(self):
+        chain = redis_client.get("blockchain")
+        if chain:
+            self.chain = self.deserialize_chain(chain)
+        if not self.chain:
+            self.create_genesis_block()  # Create genesis block if chain is empty
+
+    def hash_block(self, data):
+        block_string = json.dumps(data, sort_keys=True)
+        return hashlib.sha256(block_string.encode()).hexdigest()
 
     def create_genesis_block(self):
-        return Block(0, str(time.time()), {"message": "Genesis Block"}, "0")
+        genesis_block = Block(0, str(datetime.datetime.now()), "Genesis Block", "0")
+        self.chain.append(genesis_block)
+        self.save_chain(self.chain)
+
+
+    def serialize_chain(self):
+        blockchain = []
+        for block in self.chain:
+            blockchain.append({
+                "index": block.index,
+                "timestamp": block.timestamp,
+                "previous_hash": block.previous_hash,
+                "current_hash": block.hash,
+                "data": json.loads(block.data)
+            })
+        return json.dumps(blockchain)
+
+
+    def deserialize_chain(self, chain_data):
+        blockchain = []
+        blocks = json.loads(chain_data)
+        for blk in blocks:
+            block = Block(
+                blk["index"],
+                blk["timestamp"],
+                blk["data"],
+                blk["previous_hash"]
+            )
+            blockchain.append(block)
+        return blockchain
+
+    def save_chain(self, chain):
+        redis_client.set("blockchain", self.serialize_chain())
 
     def add_block(self, data):
         prev_block = self.chain[-1]
         new_block = Block(len(self.chain), str(time.time()), data, prev_block.hash)
         self.chain.append(new_block)
+        self.save_chain(self.chain)
 
     def verify_block(self, data):
         for block in self.chain:
@@ -59,3 +116,7 @@ class Blockchain:
             })
         return blockchain
 
+    def clear_chain(self):
+        self.chain = []
+        self.create_genesis_block()
+        self.save_chain(self.chain)
