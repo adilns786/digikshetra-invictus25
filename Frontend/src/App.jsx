7d2d493e02bd0a2ef195db9ead5d34649a5d0164
PranavPol-01 <pranav.s.pol144@gmail.com>
2025-03-01 19:33:46 +0530

    shadcn and mui config

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
   )
 }
 
